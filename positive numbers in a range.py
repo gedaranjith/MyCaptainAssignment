43ec853f5f37c1bf78b90c9e82f19ab8e3bf586e
gedaranjith <91532570+gedaranjith@users.noreply.github.com>
2022-02-08 11:25:23 +0530

    Add files via upload

---
+++ positive numbers in a range.py
@@ -0,0 +1,16 @@
+##program to print all positive numbers in a range##
+
+list1=[1,2,3,4,5,6,-7,-8,-9]
+list2=[9,8,7,6,5,-4,-3,-2,-1]
+print(" \npositive elements in list1 \n ")  
+for i in list1:
+    if i>0:
+        print(i,end=" ")
+        
+        
+print(" \npositive elements in list2 \n ")    
+for j in list2:
+    if j>0:
+        print(j,end=" ")
+       
+        
